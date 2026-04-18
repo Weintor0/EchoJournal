7ab5d29e9222a06324e44d8a790b123b2d4c56e1
Weintor <umuthancioglu@std.iyte.edu.tr>
2026-04-18 19:33:24 +0300

    Minor changes applid to entryCard

--- src/components/EntryCard.js
+++ src/components/EntryCard.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 export default function EntryCard({ entry, onPress }) {
   return (
     <TouchableOpacity style={styles.card} onPress={onPress}>
-      {/* Image Placeholder */}
-      <View style={styles.image} />
+      <View style={styles.imageSection}>
+        <View style={styles.image} />
+      </View>
 
-      {/* Content */}
-      <View style={styles.content}>
+      <View style={styles.infoSection}>
         <Text style={styles.title}>{entry.title}</Text>
 
         <Text style={styles.preview} numberOfLines={2}>
@@ -15,14 +15,15 @@ export default function EntryCard({ entry, onPress }) {
         </Text>
 
         <View style={styles.footer}>
-          <Text style={styles.meta}>
-            {entry.type} • {entry.rating}/10
-          </Text>
+          <Text style={styles.type}>{entry.type}</Text>
+          <Text style={styles.rating}>{entry.rating}/10</Text>
           <Text style={styles.date}>{entry.date}</Text>
         </View>
       </View>
 
-      <Text style={styles.details}>Details →</Text>
+      <View style={styles.detailsSection}>
+        <Text style={styles.details}>{"Details ->"}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -34,24 +35,30 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     marginBottom: 12,
-    alignItems: "center",
+    alignItems: "stretch",
+    minHeight: 90,
+  },
+  imageSection: {
+    width: "22%",
+    justifyContent: "center",
+    paddingRight: 10,
   },
   image: {
-    width: 50,
-    height: 70,
+    width: "100%",
+    aspectRatio: 0.72,
     backgroundColor: "black",
     borderRadius: 5,
-    marginRight: 10,
   },
-  content: {
-    flex: 1,
+  infoSection: {
+    width: "58%",
+    justifyContent: "space-between",
   },
   title: {
     fontWeight: "600",
-    fontSize: 14,
+    fontSize: 18,
   },
   preview: {
-    fontSize: 12,
+    fontSize: 16,
     color: "#333",
     marginVertical: 4,
   },
@@ -59,14 +66,23 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
   },
-  meta: {
-    fontSize: 12,
+  type: {
+    fontSize: 16,
+  },
+  rating: {
+    fontSize: 16,
   },
   date: {
-    fontSize: 12,
+    fontSize: 16,
+  },
+  detailsSection: {
+    width: "20%",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    paddingLeft: 6,
   },
   details: {
     fontSize: 12,
-    marginLeft: 5,
+    textAlign: "right",
   },
 });
